89e984904284a4b929f4d31b35b0f2790bca0243
silvertechmd <silvertechmd@gmail.com>
2025-11-09 15:38:21 +0500

    SILVER-Tech-MD

---
+++ settings.js
@@ -0,0 +1,28 @@
+// 🌟 Bot Configuration File 🌟
+// Edit these values to personalize your SILVER BOT
+
+const settings = {
+  // 🏷️ Sticker Settings
+  packname: '💎 SILVER-TECH-MD 💎',
+  author: '👑 SILVER MD Official 🇵🇰',
+
+  // 🤖 Bot Identity
+  botName: '⚡ SILVER TECH MD ⚡',
+  botOwner: '🌍 SILVER MD Official 🇵🇰',   // Change to your name if needed
+  ownerNumber: '923106367029',        // Your number without + symbol
+
+  // 🎬 APIs
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', // For GIF commands
+
+  // ⚙️ Bot Mode
+  commandMode: 'public', // Options: 'public' (everyone) | 'private' (owner only)
+
+  // 📝 Meta Information
+  description: '💬 A fun, powerful & stylish WhatsApp MD bot created with ❤️ by SILVER POWER.',
+  version: '2.0.0',
+updateZipUrl: "https://github.com/Shttps://github.com/silvertechmd/SILVER-Tech-MD/issues-MD/SILVER-Tech-MD/archive/refs/heads/main.zip",
+  
+};
+
+// Export so other files can use it
+module.exports = settings;
